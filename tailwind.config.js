fb65e62fdb72981e5aa414998356e28ad740c379
Andres Baeza <andresbaeza@Andress-MacBook-Pro-2.local>
2025-08-26 00:30:36 -0700

    Set up the base and folder structure for the rest of the app

---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
+  presets: [require("nativewind/preset")],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#F17720',
+        secondary: '#00A7E1',
+        white: {
+          DEFAULT: '#EBEBEB',
+          100: '#FFFFFF',
+        },
+        grey: {
+          DEFAULT: '#282828ff',
+          100: '#000000',
+        },
+        accent: {
+          orange: '#FFA630',
+          blue: '#0474BA'
+        },
+      },
+      fontFamily: {
+        quicksand: ["Quicksand-Regular", "sans-serif"],
+        "quicksand-bold": ["Quicksand-Bold", "sans-serif"],
+        "quicksand-semibold": ["Quicksand-SemiBold", "sans-serif"],
+        "quicksand-light": ["Quicksand-Light", "sans-serif"],
+        "quicksand-medium": ["Quicksand-Medium", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+}
+
